38a8e373697d82e678d22f71b5ff78e51b6a1fb1
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-16 12:30:45 -0400

    scroll

--- client/src/screens/homeScreen/HomeScreen.jsx
+++ client/src/screens/homeScreen/HomeScreen.jsx
@@ -16,17 +16,6 @@ export default function HomeScreen() {
           <Link link={"about"} title={"About"} />
           <Link link={"contact"} title={"Contact"} />
         </LinksContainer>
-        <div
-          onClick={() => {
-            window.scroll({
-              top: 1000,
-              left: 1000,
-              behavior: "smooth",
-            });
-          }}
-        >
-          Test
-        </div>
         <div>
           <img
             className={classes.icons}
