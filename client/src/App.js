76236ba97b11536929ef1ca20ba07f84b666091d
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-16 12:14:51 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import smoothscroll from "smoothscroll-polyfill";
+// import smoothscroll from "smoothscroll-polyfill";
 
 import "./App.css";
 import HomePage from "./screens/homePage/HomePage";
