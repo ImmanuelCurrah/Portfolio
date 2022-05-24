f653bd54f093c4ac08eed9a0c770a10d2e669b1b
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-24 10:33:16 -0400

    changed some wording on the about page

--- client/src/screens/about/About.jsx
+++ client/src/screens/about/About.jsx
@@ -10,9 +10,9 @@ export default function About() {
           I am a Ruby and React lover. Coming from a a background in
           construction, I have a passion for problem solving and paying close
           attention to detail. Also after having been a zen buddhist monk for a
-          number of years, I really learned what teamwork and community means. I
-          think that meeting everyone where they are at exactly is the most
-          important kind of work we can do.
+          number of years, I really learned what teamwork and community means.
+          Meeting everyone where they are at exactly is the most important kind
+          of work we can do.
         </div>
       </div>
       <div className={classes.about_container}>
