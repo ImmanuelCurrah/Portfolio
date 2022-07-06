ea13b51dd19cf11eafd22ed1a0bb7605c3e440ae
Immanuel Currah <immanueli.currah@gmail.com>
2022-07-06 10:45:24 -0400

    link is now tailwind

--- client/src/screens/Links/Link.jsx
+++ client/src/screens/Links/Link.jsx
@@ -2,8 +2,8 @@ import classes from "./Link.module.css";
 
 export default function Link(props) {
   return (
-    <li>
-      <a href={`#${props.link}`} className={classes.links}>
+    <li className="my-2">
+      <a href={`#${props.link}`} className="text-2xl">
         {props.title}
       </a>
     </li>
