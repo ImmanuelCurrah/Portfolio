123b2900c003e30d97cb4a6da6b0cba7179016f1
Immanuel Currah <immanueli.currah@gmail.com>
2022-07-06 14:32:15 -0400

    modal is now tailwind

--- client/src/Modal/Modal.jsx
+++ client/src/Modal/Modal.jsx
@@ -1,19 +1,25 @@
-import "./Modal.css";
-
 export default function Modal(props) {
   return (
     <div
-      className={`modal ${props.show ? "show" : ""}`}
+      className={`${
+        props.show
+          ? "h-screen flex flex-col items-center justify-center ease-in-out duration-300 fixed top-0 left-0 right-0 opacity-1 bg-[#00000080]"
+          : "h-screen flex flex-col items-center justify-center ease-in-out duration-300 fixed top-0 left-0 right-0 opacity-0 bg-[#00000080] pointer-events-none"
+      }`}
       onClick={() => props.onClose()}
     >
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <div className="modal-header">
+      <div
+        className="flex flex-col items-center rounded-lg bg-[#fffff9] shadow-black shadow-lg w-10/12"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="text-xl p-4">
           <h4 className="modal-title">{props.title}</h4>
         </div>
-        <div className="modal-body">{props.children}</div>
-        <div className="modal-footer"></div>
+        <div className="p-4 my-4 border-y-2 border-[#eee]">
+          {props.children}
+        </div>
         <button
-          className="button"
+          className="text-white bg-black font-lg w-24 h-12 rounded-lg m-2 cursor-pointer"
           onClick={() => {
             props.onClose();
           }}
