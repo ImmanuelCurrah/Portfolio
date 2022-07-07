6529409561f270f0e479e54483c2caab67d58bf3
Immanuel Currah <immanueli.currah@gmail.com>
2022-07-07 11:43:34 -0400

    project page refactor

--- client/src/Projects/Project/Project.jsx
+++ client/src/Projects/Project/Project.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import Modal from "../../Modal/Modal";
 import Card from "../../UI/Card/Card";
 
@@ -6,14 +7,20 @@ export default function Project(props) {
   const [show, setShow] = useState(false);
   return (
     <div className="flex flex-row items-center h-16 text-center cursor-pointer">
-      <h2
-        className="text-3xl font-medium"
-        onClick={() => {
-          setShow(true);
-        }}
-      >
-        {props.projectName}
-      </h2>
+      <div className="flex flex-col items-start justify-center group">
+        <span className="opacity-0 group-hover:opacity-100 group-hover:animate-bounce">
+          <BsFillArrowDownCircleFill />
+        </span>
+        <h2
+          className="text-3xl font-medium transition duration-150 border-b-2 border-transparent hover:border-black"
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          {props.projectName}
+        </h2>
+      </div>
+
       <Modal
         onClose={() => {
           setShow(false);
